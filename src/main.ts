417762c6af4b4d2ed18dbbdd03054d5b61ade41d
unknown <g00420095@atu.ie>
2024-04-19 12:39:23 +0000

    Lab8 Complete

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { importProvidersFrom } from '@angular/core';
+
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
@@ -16,5 +19,6 @@ bootstrapApplication(AppComponent, {
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes),
+    importProvidersFrom(IonicStorageModule.forRoot())
   ],
 });
